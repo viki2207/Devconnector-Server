7691a879330329ab2095f4ece7afbdd0dbd9cf26
viki2207 <vidhishah2207@gmail.com>
2023-10-22 09:18:24 +1100

    seperate backend and frontend

--- Backend/server.js
+++ Backend/server.js
@@ -27,6 +27,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const PORT = process.env.PORT || 5016;
+const PORT = process.env.PORT || 5017;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
