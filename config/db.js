a028ce8808ab6de37a8ecab78be81760026628d5
viki2207 <vidhishah2207@gmail.com>
2023-10-21 19:22:23 +1100

    Devconnector server

---
+++ config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const config = require('config');
+//const db = 'mongodb://localhost:27017';
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017', {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    });
+
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
