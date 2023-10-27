1a2c3bcd2b08488842ba4890488cc4a4eed93e99
viki2207 <vidhishah2207@gmail.com>
2023-10-27 22:38:59 +1100

    working check

--- Backend/server.js
+++ Backend/server.js
@@ -3,25 +3,26 @@ const connectDB = require('./html_css_theme/config/db');
 const path = require('path');
 
 const app = express();
-
-// Connect Database
-connectDB();
-
-// Init Middleware
 app.use(express.json());
+// Connect Database
 
+app.use(cors());
 // Define Routes
 app.use('/api/users', require('./html_css_theme/routes/api/users'));
 app.use('/api/auth', require('./html_css_theme/routes/api/auth'));
 app.use('/api/profile', require('./html_css_theme/routes/api/profile'));
 app.use('/api/posts', require('./html_css_theme/routes/api/posts'));
+connectDB();
+
+// Init Middleware
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
   // app.use(cores());
-  app.get('*', (req, res) => {
+  app.get('/health', (req, res) => {
+    res.send("Working");
     //res.setHeader('Access-control-allow-Credentials', true);
     //res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
