ad09edabd8df777dc4b9f49e2587fecc8ba375bd
viki2207 <vidhishah2207@gmail.com>
2023-10-27 22:44:26 +1100

    add cors lib

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./html_css_theme/config/db');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
@@ -22,7 +23,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   // app.use(cores());
   app.get('/health', (req, res) => {
-    res.send("Working");
+    res.send('Working');
     //res.setHeader('Access-control-allow-Credentials', true);
     //res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
